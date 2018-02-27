6e0626eacd8342d5c860cf516bc62451b670f387
Timo Klaavo <timo@klaavo.com>
2018-02-27 21:48:50 +0200

    Small tweaks and cleanup for RF3

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Scaling Edit Tool
 =================
-Custom editing tool extension for RoboFont editor - version 1.0.1
+Custom editing tool extension for RoboFont editor - version 1.0.2
 
 Scaling Edit Tool for RoboFont is a mouse and keyboard controlled version of what are better known
-as Interpolated Nudge tools, [made famous by Christian Robertson](http://betatype.com/node/18).
+as Interpolated Nudge tools, made famous by [Christian Robertson](http://www.christianrobertson.com).
 When moving oncurve points, offcurve points are scaled in relation to neighboring oncurve points.
 
 The tool works on both bicubic and quadratic bezier curves, and open and closed contours.
@@ -13,6 +13,5 @@ Angles of offcurve points are retained by default. Command-key overrides the ang
 mouse is down. In contextual menu (right mouse click) you can choose whether or not smooth and
 non-selected points are affected by the override. Simplified mode offers a more traditional
 operation without angle keeping.
-
 
 [MIT Licence](https://en.wikipedia.org/wiki/MIT_License) Copyright (C) 2012 Timo Klaavo
--- ScalingEditTool.roboFontExt/lib/scalingEditToolExt.py
+++ ScalingEditTool.roboFontExt/lib/scalingEditToolExt.py
@@ -1,27 +1,32 @@
 """
-Scaling Edit Tool for RoboFont is a mouse and keyboard controlled version of what are better known
-as Interpolated Nudge tools, made famous by Christian Robertson http://betatype.com/node/18
-When moving oncurve points, offcurve points are scaled in relation to neighboring oncurve points.
-The tool works on both bicubic and quadratic bezier curves, and open and closed contours.
-All the built-in Edit Tool functionality should work as expected.
-Angles of offcurve points are retained by default. Command-key overrides the angle keeping when
-mouse is down. In contextual menu (right mouse click) you can choose whether or not smooth and
-non-selected points are affected by the override. Simplified mode offers a more traditional
-operation without angle keeping. Timo Klaavo 2012-2016 v. 1.0.1
+Scaling Edit Tool for RoboFont is a mouse and keyboard controlled version
+of what are better known as Interpolated Nudge tools, made famous by
+Christian Robertson http://www.christianrobertson.com
+When moving oncurve points, offcurve points are scaled in relation to
+neighboring oncurve points. The tool works on both bicubic and quadratic
+bezier curves, and open and closed contours. All the built-in Edit Tool
+functionality should work as expected. Angles of offcurve points are retained
+by default. Command-key overrides the angle keeping when mouse is down.
+In contextual menu (right mouse click) you can choose whether or not smooth and
+non-selected points are affected by the override. Simplified mode offers
+a more traditional operation without angle keeping.
 """
 
 from mojo.events import EditingTool, installTool, setActiveEventTool
 from mojo.extensions import getExtensionDefault, setExtensionDefault
+from mojo.roboFont import version
 from AppKit import NSImage
 from math import sqrt
-import os
-from mojo.roboFont import version
+from os import path
 
-dirname = os.path.dirname(__file__)
-toolbarIcon = NSImage.alloc().initByReferencingFile_(os.path.join(dirname, "scalingEditToolbarIcon.pdf"))
+
+dirname = path.dirname(__file__)
+toolbarIcon = NSImage.alloc().initByReferencingFile_(path.join(dirname, "scalingEditToolbarIcon.pdf"))
+
 
 def diff(a, b, c=0):
     return float(abs(a - b)) if c == 0 else float(a - b)
+
 
 def pointData(p1, p2, p1Ut, p2In, simplified):
     # distances between points:
@@ -51,6 +56,7 @@ def pointData(p1, p2, p1Ut, p2In, simplified):
     # 4 points, 4 bcp-distance-ratios, 4 x-y-ratios, 4 directions,
     return p1, p2, p1Ut, p2In, p1xr, p1yr, p2xr, p2yr, p1yx, p1xy, p2yx, p2xy, p1dx, p1dy, p2dx, p2dy
 
+
 def smoothLines(p, pp, offCurve):
     # bcp length from relative coordinates
     bcp = offCurve.x - p.x, offCurve.y - p.y
@@ -72,6 +78,7 @@ def smoothLines(p, pp, offCurve):
     # new absolute coordinates
     return newX * ldrx + p.x, newY * ldry + p.y
 
+
 def keepAngles(p, offCurve, pyx, pxy, pdx, pdy):
     # bcp length from relative coordinates:
     bcp = offCurve.x - p.x, offCurve.y - p.y
@@ -91,11 +98,10 @@ class ScalingEditTool(EditingTool):
     def getToolbarTip(self):
         return "Scaling Edit"
 
-    def additionContectualMenuItems(self):  #  Up to RF 1.6
+    def additionContectualMenuItems(self):  # Up to RF 1.6
         selectText = 'Turn On Non-Selected' if self.settings['selectOnly'] else 'Turn Off Non-Selected'
         smoothText = 'Turn Off Smooths' if self.settings['smoothsToo'] else 'Turn On Smooths'
         simpliText = 'Turn Off Simplified Mode' if self.settings['simplified'] else 'Turn On Simplified Mode'
-        setActiveEventTool('ScalingEditTool') # needs a reset if using trackpad for some reason. This bug is in the default Edit Tool also...
         return [('Angle Keeping Override (cmd-key)', [(selectText, self.menuCallSelected), (smoothText, self.menuCallSmooths)]), (simpliText, self.menuCallSimplified)]
 
     def additionContextualMenuItems(self):  # RF 1.7
@@ -135,63 +141,61 @@ class ScalingEditTool(EditingTool):
     def mouseDown(self, point, clickCount):
         self.buildScaleDataList()
 
-    def mouseUp(self, point): # for lasso selections
+    def mouseUp(self, point):  # for lasso selections
         self.buildScaleDataList()
 
     def mouseDragged(self, point, delta):
-        if not self.optionDown and not self.commandDown: # allow default option and command behavior
+        if not self.optionDown and not self.commandDown:  # allow default option and command behavior
             self.scalePoints()
 
     def modifiersChanged(self):
-        if self.isDragging(): # command-key override of angle keeping works only when mouse is down
+        if self.isDragging():  # command-key override of angle keeping works only when mouse is down
             self.scalePoints()
-            # RF3
-            if version >= "3.0.0":
+            if version >= '2.0.0':  # RF2 and later
                 self.glyph.changed()
-            # RF1
-            else:
+            else:  # RF1
                 self.glyph.update()
 
     def keyDown(self, event):
         if any(self.arrowKeysDown[i] for i in self.arrowKeysDown):
             self.scalePoints(arrowKeyDown=True)
-        elif not self.isDragging() or self.isDragging() and event.keyCode() == 48: # 48 = tab or modifier+tab
-            self.buildScaleDataList() # triggered by tab while dragging, and all keys except arrows while not dragging
+        elif not self.isDragging() or self.isDragging() and event.keyCode() == 48:  # 48 = tab or modifier+tab
+            self.buildScaleDataList()  # triggered by tab while dragging, and all keys except arrows while not dragging
 
     def buildScaleDataList(self, selectionChanged=0):
         self.scaleData = []
-        if self.glyph and self.glyph.selection != []: # stop if there is nothing selected
+        if self.glyph and self.glyph.selection != []:  # stop if there is nothing selected
             for cI in range(len(self.glyph.contours)):
-                if len(self.glyph.contours[cI]) > 1: # skip lonesome points
+                if len(self.glyph.contours[cI]) > 1:  # skip lonesome points
                     contr = self.glyph.contours[cI]
                     segms = contr.segments[:]
-                    segms = segms[:-1] if segms[-1].type == 'offCurve' else segms # ignore tailing 'offCurve'-segments in open contours
-                    segms = segms[1:] + segms[:1] if segms[0].type == 'move' else segms # 'move'-segment of open contours from beginning to end
+                    segms = segms[:-1] if segms[-1].type == 'offCurve' else segms  # ignore tailing 'offCurve'-segments in open contours
+                    segms = segms[1:] + segms[:1] if segms[0].type == 'move' else segms  # 'move'-segment of open contours from beginning to end
                     for pI in range(len(segms)):
-                        if 'c' in segms[pI-2].type: # 'c' is in 'curve' and 'qcurve', not in 'offCurve', 'line' or 'move'
-                            i3 = 3 if len(segms) > 2 else 1 # cheat with indexes if only 2 points in contour
-                            p1 = segms[pI-i3].points[-1] # point in beginning of curve to be scaled
-                            p2 = segms[pI-2].points[-1] # ending point of curve to be scaled
+                        if segms[pI-2].type == 'curve' or segms[pI-2].type == 'qcurve':  # not 'offCurve', 'line', 'move' or such
+                            i3 = 3 if len(segms) > 2 else 1  # cheat with indexes if only 2 points in contour
+                            p1 = segms[pI - i3].points[-1]  # point in beginning of curve to be scaled
+                            p2 = segms[pI - 2].points[-1]  # ending point of curve to be scaled
                             if p1.selected and not p2.selected or p2.selected and not p1.selected:
-                                p3 = segms[pI-1].points[-1] # next onCurve point after p2
-                                p0 = segms[pI-i3-1].points[-1] if len(segms) != 3 else p3 # previous onCurve point, p0 is p3 in 3-point outline
-                                p1Ut = segms[pI-2].points[-3] # out-point of p1
-                                p2In = segms[pI-2].points[-2] # in-point of p2
-                                prevType = segms[pI-i3].type # previous segment type
-                                nextType = segms[pI-1].type # next segment type
+                                p3 = segms[pI - 1].points[-1]  # next onCurve point after p2
+                                p0 = segms[pI - i3 - 1].points[-1] if len(segms) != 3 else p3  # previous onCurve point, p0 is p3 in 3-point outline
+                                p1Ut = segms[pI - 2].points[-3]  # out-point of p1
+                                p2In = segms[pI - 2].points[-2]  # in-point of p2
+                                prevType = segms[pI - i3].type  # previous segment type
+                                nextType = segms[pI - 1].type  # next segment type
                                 self.scaleData.append(pointData(p1, p2, p1Ut, p2In, self.settings['simplified']) + (p0, p3, prevType, nextType))
 
     def scalePoints(self, arrowKeyDown=0):
-        if not self.transformMode(): # normal behavior in transform mode
+        if not self.transformMode():  # normal behavior in transform mode
             for i in self.scaleData:
 
-                p1, p2 = i[0], i[1] # two onCurve points of the segment to be scaled
-                p1Ut, p2In = i[2], i[3] # out and in offCurve points of the curve
+                p1, p2 = i[0], i[1]  # two onCurve points of the segment to be scaled
+                p1Ut, p2In = i[2], i[3]  # out and in offCurve points of the curve
                 p1xr, p1yr, p2xr, p2yr = i[4], i[5], i[6], i[7]
                 p1yx, p1xy, p2yx, p2xy = i[8], i[9], i[10], i[11]
                 p1dx, p1dy, p2dx, p2dy = i[12], i[13], i[14], i[15]
-                p0, p3 = i[16], i[17] # previous and next onCurve points
-                prevType, nextType = i[18], i[19] # previous and next segment types
+                p0, p3 = i[16], i[17]  # previous and next onCurve points
+                prevType, nextType = i[18], i[19]  # previous and next segment types
 
                 # scale curve
                 newDistX = diff(p1.x, p2.x, self.settings['simplified'])
@@ -202,23 +206,24 @@ class ScalingEditTool(EditingTool):
 
                 # correct offCurve angles
                 if not self.settings['simplified']:
-                    if prevType == 'line' and p1.smooth: # smooth line before
+                    if prevType == 'line' and p1.smooth:  # smooth line before
                         p1Ut.x, p1Ut.y = smoothLines(p1, p0, p1Ut)
-                    elif p1yx: # diagonal p1Ut
+                    elif p1yx:  # diagonal p1Ut
                         p1Ut.x, p1Ut.y = keepAngles(p1, p1Ut, p1yx, p1xy, p1dx, p1dy)
-                        if not arrowKeyDown and self.commandDown: # keep angle override
+                        if not arrowKeyDown and self.commandDown:  # keep angle override
                             if self.settings['smoothsToo'] or not self.settings['smoothsToo'] and not p1.smooth:
                                 if self.settings['selectOnly'] and p1.selected or not self.settings['selectOnly']:
                                     p1Ut.x, p1Ut.y = p1UtX, p1UtY
 
                     if nextType == 'line' and p2.smooth:  # smooth line after
                         p2In.x, p2In.y = smoothLines(p2, p3, p2In)
-                    elif p2yx: # diagonal p2In
+                    elif p2yx:  # diagonal p2In
                         p2In.x, p2In.y = keepAngles(p2, p2In, p2yx, p2xy, p2dx, p2dy)
-                        if not arrowKeyDown and self.commandDown: # keep angle override
+                        if not arrowKeyDown and self.commandDown:  # keep angle override
                             if self.settings['smoothsToo'] or not self.settings['smoothsToo'] and not p2.smooth:
                                 if self.settings['selectOnly'] and p2.selected or not self.settings['selectOnly']:
                                     p2In.x, p2In.y = p2InX, p2InY
 
 
 installTool(ScalingEditTool())
+setActiveEventTool('ScalingEditTool')
