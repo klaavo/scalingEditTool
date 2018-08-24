eb99574d56c427cf777531ea33a210be600601bb
Gustavo Ferreira <gustavo@hipertipo.com>
2018-08-24 07:48:40 -0300

    making image path relative, adding white background to icon (Mechanic2)

--- mechanic2/makeIconMechanic.py
+++ mechanic2/makeIconMechanic.py
@@ -1,13 +1,15 @@
 import os
 
-imgPath = '/_code/_extensions/scalingEditTool/ScalingEditTool.roboFontExt/lib/ScalingEditToolbarIcon.pdf'
+folder = os.getcwd()
+baseFolder = os.path.dirname(folder)
+imgPath = os.path.join(baseFolder, 'ScalingEditTool.roboFontExt/lib/ScalingEditToolbarIcon.pdf')
 
 size(300, 300)
+fill(1)
+rect(0, 0, width(), height())
 translate(21, 21)
 scale(14)
 image(imgPath, (0, 0))
 
-folder = os.path.split(imgPath)[0]
 outputPath = os.path.join(folder, 'ScalingEditMechanicIcon.png')
-
 saveImage(outputPath)
